1acf67acf47e6781d1f7918c6fcd858928185ef1
code <code@localhost.localdomain>
2019-07-23 20:46:57 +0800

    增加各个级别的宏定义，方便使用

--- lib/log4lib.h
+++ lib/log4lib.h
@@ -45,4 +45,64 @@ private:
         }\
     }while(0);
 
+//CLog_DEBUG
+#define DTLOG(tstr) do{\
+        if(g_CLog_init){\
+            std::ostringstream ostr;\
+            ostr<<"{"<<__FILE__<<" - "<<__LINE__<<"}: "<<tstr;\
+            CLog::getIns()->write(CLog_DEBUG, ostr.str());\
+        }\
+        else{\
+            std::cout<<"log never init"<<std::endl;\
+        }\
+    }while(0);
+
+//CLog_INFO
+#define ITLOG(tstr) do{\
+        if(g_CLog_init){\
+            std::ostringstream ostr;\
+            ostr<<"{"<<__FILE__<<" - "<<__LINE__<<"}: "<<tstr;\
+            CLog::getIns()->write(CLog_INFO, ostr.str());\
+        }\
+        else{\
+            std::cout<<"log never init"<<std::endl;\
+        }\
+    }while(0);
+
+//CLog_WARN
+#define WTLOG(tstr) do{\
+        if(g_CLog_init){\
+            std::ostringstream ostr;\
+            ostr<<"{"<<__FILE__<<" - "<<__LINE__<<"}: "<<tstr;\
+            CLog::getIns()->write(CLog_WARN, ostr.str());\
+        }\
+        else{\
+            std::cout<<"log never init"<<std::endl;\
+        }\
+    }while(0);
+
+//CLog_ERROR
+#define ETLOG(tstr) do{\
+        if(g_CLog_init){\
+            std::ostringstream ostr;\
+            ostr<<"{"<<__FILE__<<" - "<<__LINE__<<"}: "<<tstr;\
+            CLog::getIns()->write(CLog_ERROR, ostr.str());\
+        }\
+        else{\
+            std::cout<<"log never init"<<std::endl;\
+        }\
+    }while(0);
+
+//CLog_FATAL
+#define FTLOG(tstr) do{\
+        if(g_CLog_init){\
+            std::ostringstream ostr;\
+            ostr<<"{"<<__FILE__<<" - "<<__LINE__<<"}: "<<tstr;\
+            CLog::getIns()->write(CLog_FATAL, ostr.str());\
+        }\
+        else{\
+            std::cout<<"log never init"<<std::endl;\
+        }\
+    }while(0);
+    
 #endif //_H_LOG4LIB_
